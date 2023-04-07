ab6343f183d2012824dbe04cadf40065a844db1a
andreluismiller <46939182+andreluismiller@users.noreply.github.com>
2023-04-07 18:28:32 -0300

    models commit

--- page/admin.py
+++ page/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile, Page, Posts
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Page)
+admin.site.register(Posts)
