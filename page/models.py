09b8e135a5b9c01dc06cca893fa793bb23ee9801
andreluismiller <46939182+andreluismiller@users.noreply.github.com>
2023-04-07 16:59:38 -0300

    initial commit

---
+++ page/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# class Creator:
+#     pass
+#     id = models.PositiveBigIntegerField(primary_key=True)
+#     username =
+
+# class Page():
+#     pass
+#     userId = models.OneToOneField(Creator, on_delete=)
+
+
+# class Post():
+#     userId = models.ForeignKey(Creator, on_delete=..)
